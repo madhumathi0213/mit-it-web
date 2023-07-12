be2546efd710678476daa951312e5cfbe65c8a93
madhumathi_allimuthu <madhumathi@pentafox.in>
2023-07-12 10:50:41 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `mit-it-web`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
